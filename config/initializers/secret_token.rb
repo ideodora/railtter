40ca78e555b33b65b3605ba6e424d3565caae4e2
ideodora <ideodora@gmail.com>
2012-12-04 10:42:08 +0900

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Momocloanalytics::Application.config.secret_token = 'd6483835faaa56569bb6d15641ca3190bdf7e547b0f41d529860288354d1921d0ba33bb0297cde70d3645109d422ac9e466fa48407a78d91740fa715274d94ff'
